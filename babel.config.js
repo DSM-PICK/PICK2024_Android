12d9bc811c2b3b30572cb17a4b6ab57b841604f1
six-standard <dbrrlwns1127@gmail.com>
2024-02-02 23:31:47 +0900

    chore: 절대경로 지정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./app"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "@": "./app",
+            "@components": "./app/components",
+            "@screens": "./app/screens",
+            "@assets": "./app/assets",
+            "@icons": "./app/assets/icons",
+          },
+        },
+      ],
+    ],
   };
 };
