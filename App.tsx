221100dd1107d8eaa9a60cf74e3f1fee37eb9b9e
six-standard <dbrrlwns1127@gmail.com>
2024-02-02 23:37:29 +0900

    chore: App 코드 정리

--- App.tsx
+++ App.tsx
@@ -1,30 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import Text from "./app/components/common/Text";
-import Input from "./app/components/common/Input";
+import MyStack from "@/navigation/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text type="heading" size={5}>
-        Test
-      </Text>
-      <Input
-        placeholder="text field"
-        value=""
-        onChange={({ text, name }) => {}}
-        name="test"
-        error={true}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MyStack />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
