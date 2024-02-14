a844b152c7de5e2eb513dbb549a3682858d0c5a4
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:35:57 +0900

    feat: 로그인 화면 오류메세지 추가 겸해서 코드 정리

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,16 +1,22 @@
-import Button from "@/components/common/Button";
-import Input from "@/components/common/Input";
-import Layout from "@/components/common/Layout";
-import Text from "@/components/common/Text";
-import { useState } from "react";
-import { View } from "react-native";
 import { StyleSheet } from "react-native";
+import { View } from "react-native";
+import { useState } from "react";
+import Button from "@/components/common/Button";
+import Layout from "@/components/common/Layout";
+import Input from "@/components/common/Input";
+import Text from "@/components/common/Text";
+import { getEmpty } from "@/utils/getEmpty";
 
 export const Login = () => {
   const [data, setData] = useState({
     accountID: "",
     password: "",
   });
+  const [error, setError] = useState({
+    accountID: false,
+    password: false,
+  });
+  const disabled = getEmpty(data.accountID) || getEmpty(data.password);
 
   const handleChange = ({ text, name }) => {
     setData({ ...data, [name]: text });
@@ -25,21 +31,50 @@ export const Login = () => {
         <Text type="body" size={2} color={["neutral", 400]}>
           스퀘어 계정으로 로그인해주세요.
         </Text>
-        <Input
-          value={data.accountID}
-          onChange={handleChange}
-          name="accountID"
-          placeholder="아이디"
-        />
-        <Input
-          value={data.accountID}
-          onChange={handleChange}
-          name="accountID"
-          placeholder="아이디"
-        />
+        <View style={styles.innerInputContainer}>
+          <Input
+            value={data.accountID}
+            onChange={handleChange}
+            name="accountID"
+            placeholder="아이디"
+            error={error.accountID}
+          />
+          <Text
+            type="caption"
+            size={2}
+            color={["error", 400]}
+            hidden={!error.accountID}
+          >
+            아이디를 다시 확인해 주세요
+          </Text>
+        </View>
+
+        <View style={styles.innerInputContainer}>
+          <Input
+            value={data.password}
+            onChange={handleChange}
+            name="password"
+            placeholder="비밀번호"
+            error={error.password}
+            password
+          />
+          <Text
+            type="caption"
+            size={2}
+            color={["error", 400]}
+            hidden={!error.password}
+          >
+            비밀번호를 다시 확인해 주세요
+          </Text>
+        </View>
       </View>
       <View style={styles.buttonContainer}>
-        <Button onPress={() => {}} disabled>
+        <Button
+          size="full"
+          color="primary"
+          onPress={() => {}}
+          disabled={disabled}
+        >
           로그인
         </Button>
       </View>
@@ -50,8 +85,11 @@ export const Login = () => {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 5,
-    gap: 20,
+    gap: 15,
     justifyContent: "center",
+  },
+  innerInputContainer: {
+    gap: 5,
   },
   buttonContainer: {
     flex: 1,
