60b04e1ed59ed22b72acac7b8e150aa41b7f12a1
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:28:38 +0900

    feat: 헤더 추가 겸해서 코드 정리

--- app/components/common/Layout.tsx
+++ app/components/common/Layout.tsx
@@ -1,23 +1,82 @@
-import { getColors } from "@/utils/functions/getColors";
-import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { getColors } from "@/utils/colors";
+import { StyleSheet, View } from "react-native";
+import { Back } from "@/assets/icons";
+import Text from "./Text";
 
 interface PropTypes {
   children: React.ReactNode;
   home?: boolean;
+  name?: string;
+  doneAction?: () => void;
+  noHorizontalPadding?: boolean;
 }
 
-export default function Layout({ children, home }: PropTypes) {
+const hitSlop = { top: 10, left: 10, right: 10, bottom: 10 };
+
+export default function Layout({
+  children,
+  home,
+  name,
+  doneAction,
+  noHorizontalPadding,
+}: PropTypes) {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        paddingHorizontal: 24,
-        paddingVertical: 15,
-        backgroundColor: home ? getColors(["primary", 1100]) : "white",
+        backgroundColor: home ? getColors(["primary", 1000]) : "white",
+        paddingVertical: !name && 25,
       }}
     >
-      {children}
+      {name && (
+        <View style={styles.headerContainer}>
+          <Back
+            style={styles.backElement}
+            onPress={() => navigation.goBack()}
+            hitSlop={hitSlop}
+          />
+          <Text type="subTitle" size={3} weight="M">
+            {name}
+          </Text>
+          {doneAction && (
+            <View style={styles.doneElement}>
+              <Text type="subTitle" size={3} weight="M" onPress={() => {}}>
+                확인
+              </Text>
+            </View>
+          )}
+        </View>
+      )}
+      <View style={{ paddingHorizontal: !noHorizontalPadding && 25 }}>
+        {children}
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    marginVertical: 10,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backElement: {
+    position: "absolute",
+    left: 25,
+  },
+  doneElement: {
+    position: "absolute",
+    right: 25,
+  },
+});
