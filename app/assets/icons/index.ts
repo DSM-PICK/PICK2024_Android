60d0d7fe7553573f6742769e08e7f167dd3b4c7f
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:31:29 +0900

    add: 아이콘 파일 추가

---
+++ app/assets/icons/index.ts
@@ -0,0 +1,25 @@
+import Apply from "./apply.svg";
+import Bell from "./bell.svg";
+import EyeOff from "./eyeOff.svg";
+import EyeOn from "./eyeOn.svg";
+import Meals from "./meal.svg";
+import My from "./my.svg";
+import Schedule from "./schedule.svg";
+import Settings from "./settings.svg";
+import NoticeIcon from "./notice.svg";
+import New from "./new.svg";
+import Back from "./back.svg";
+
+export {
+  Apply,
+  Bell,
+  EyeOff,
+  EyeOn,
+  Meals,
+  My,
+  Schedule,
+  Settings,
+  NoticeIcon,
+  New,
+  Back,
+};
