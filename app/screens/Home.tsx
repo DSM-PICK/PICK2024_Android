fc34bf0ba9111d32d43eb7addc2ee8a533bcc3bd
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:34:16 +0900

    feat: 홈 화면 퍼블리싱

---
+++ app/screens/Home.tsx
@@ -0,0 +1,82 @@
+import { View, StyleSheet } from "react-native";
+import { Apply, Schedule, Meals, My, Bell, Settings } from "@assets/icons";
+import HomeButton from "@/components/home/HomeButton";
+import Carousel from "@/components/common/Carousel";
+import TimeTable from "@/components/home/TimeTable";
+import Layout from "@/components/common/Layout";
+import Notice from "@/components/home/Notice";
+import Text from "@/components/common/Text";
+import Meal from "@/components/home/Meal";
+
+const headerIconSize = 16;
+const buttonIconSize = { width: 30, height: 30 };
+
+export const Home = ({ navigation }) => {
+  return (
+    <Layout home noHorizontalPadding>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.multiContainer}>
+            <Text type="heading" size={6} weight="B" color={["primary", 300]}>
+              PiCK
+            </Text>
+            <View style={styles.headerButtonContainer}>
+              <Bell width={headerIconSize} />
+              <Settings width={headerIconSize} />
+            </View>
+          </View>
+          <Text type="subTitle" size={2} weight="B" color={["neutral", 50]}>
+            1학년 3반 11번 육기준
+          </Text>
+          <View style={styles.multiContainer}>
+            <HomeButton
+              icon={<Schedule {...buttonIconSize} />}
+              name="일정"
+              nav={navigation}
+            />
+            <HomeButton
+              icon={<Apply {...buttonIconSize} />}
+              name="신청"
+              nav={navigation}
+            />
+            <HomeButton
+              icon={<Meals {...buttonIconSize} />}
+              name="급식"
+              nav={navigation}
+            />
+            <HomeButton
+              icon={<My {...buttonIconSize} />}
+              name="My"
+              nav={navigation}
+            />
+          </View>
+        </View>
+        <Carousel height={450}>
+          <TimeTable />
+          <Meal />
+          <Notice />
+        </Carousel>
+      </View>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 20,
+  },
+  topContainer: {
+    paddingHorizontal: 25,
+    gap: 20,
+  },
+  multiContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  headerButtonContainer: {
+    flexDirection: "row",
+    gap: 10,
+  },
+});
