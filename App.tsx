aaadd500d92ea38f725a49293afc15455f750170
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:26:47 +0900

    perf: 네비게이팅 최적화 react-native-screen을 이용하여 네비게이팅 최적화

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import MyStack from "@/navigation/Navigation";
+import { enableScreens } from "react-native-screens";
 
 export default function App() {
+  enableScreens(false);
   return <MyStack />;
 }
