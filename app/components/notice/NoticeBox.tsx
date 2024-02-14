b289ebd340ac8ef8937ebe97d14792c4e9ddabc3
six-standard <dbrrlwns1127@gmail.com>
2024-02-14 23:34:55 +0900

    feat: 공지사항 화면 퍼블리싱

---
+++ app/components/notice/NoticeBox.tsx
@@ -0,0 +1,54 @@
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { New, NoticeIcon } from "@/assets/icons";
+import { getDiff } from "@/utils/getDiff";
+import Text from "../common/Text";
+import Box from "../common/Box";
+interface PropType {
+  title: string;
+  date: string;
+  index: number;
+}
+
+export default function NoticeBox({ title, date, index }: PropType) {
+  const navigation = useNavigation();
+
+  return (
+    <Box color="white" rounded="none">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("상세공지", { id: index })}
+        style={styles.container}
+      >
+        <NoticeIcon />
+        <View style={styles.textContainer}>
+          <View style={styles.titleContainer}>
+            <Text type="body" size={2}>
+              {title}
+            </Text>
+            {!!!index && <New />}
+          </View>
+          <Text type="caption" size={2} color={["neutral", 200]}>
+            {getDiff(date)}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Box>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignContent: "center",
+    gap: 5,
+  },
+  textContainer: {
+    gap: 5,
+  },
+});
---
+++ app/screens/Notice.tsx
@@ -0,0 +1,37 @@
+import { FlatList } from "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import NoticeBox from "@/components/notice/NoticeBox";
+import Layout from "@/components/common/Layout";
+import { getColors } from "@/utils/colors";
+import { noticeData } from "@/tmpData";
+
+export const Notice = () => {
+  return (
+    <Layout name="공지사항">
+      <View style={styles.container}>
+        <FlatList
+          overScrollMode="never"
+          ItemSeparatorComponent={() => (
+            <View style={styles.separatorElement} />
+          )}
+          data={noticeData}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <NoticeBox title={item.title} date={item.date} index={index} />
+          )}
+        />
+      </View>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "95%",
+  },
+  separatorElement: {
+    width: "100%",
+    height: 0.5,
+    backgroundColor: getColors(["primary", 900]),
+  },
+});
