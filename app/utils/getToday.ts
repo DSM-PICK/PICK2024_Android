19c47f29bedef29c6818afdca89cd5fb1a1ce736
six-standard <dbrrlwns1127@gmail.com>
2024-02-19 19:29:02 +0900

    refactor: 요일 값 export

--- app/utils/getToday.ts
+++ app/utils/getToday.ts
@@ -1,6 +1,7 @@
+export const days = ["일", "월", "화", "수", "목", "금", "토"];
+
 export const getToday = () => {
   const _date = new Date();
-  const days = ["일", "월", "화", "수", "목", "금", "토"];
 
   const year = _date.getFullYear();
   const month = _date.getMonth() + 1;
