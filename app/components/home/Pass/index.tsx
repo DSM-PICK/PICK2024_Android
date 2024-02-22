8380bd5fe709c54c2ae4fbd1b5886b2dd7ff688b
six-standard <dbrrlwns1127@gmail.com>
2024-02-22 18:15:14 +0900

    feat: 외출증 컴포넌트 퍼블리싱

---
+++ app/components/home/Pass/index.tsx
@@ -0,0 +1,33 @@
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, View } from "react-native";
+import Box from "../../common/Box";
+import Home from "./Home";
+import Out from "./Out";
+import Move from "./Move";
+
+const Types = {
+  home: ({ name, data }) => Home({ name: name, date: data }),
+  out: ({ name, data }) => Out({ name: name, date: data }),
+  move: ({ name, data }) => Move({ name: name, place: data }),
+};
+
+export default function Pass({ visible, type, name, data }) {
+  const navigation = useNavigation();
+
+  if (visible) {
+    return (
+      <Box>
+        <View style={styles.container}>{Types[type]({ name, data })}</View>
+      </Box>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
