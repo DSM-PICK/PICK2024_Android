354f547e9b7cace22bac99d35fbd980f7bfdebee
six-standard <dbrrl1127@gmail.com>
2024-03-11 23:15:23 +0900

    feat: 자습감독 일부 연동

--- app/screens/Teacher/index.tsx
+++ app/screens/Teacher/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, View } from "react-native";
 import { useState } from "react";
 import { Calendar, Text } from "@commonents";
 import { queryKeys } from "@/constants";
-import { Layout, Box } from "@layouts";
+import { Layout, Box, HiddenView } from "@layouts";
 import { getToday } from "@/utils";
 import { today } from "@/api";
 
@@ -12,12 +12,16 @@ const { year, month, date: _date } = getToday();
 export const Teacher = () => {
   const [selected, setSelected] = useState(`${month}월 ${_date}일`);
 
-  const { data: teacherData } = useQuery({
-    queryKey: queryKeys.teacher,
+  const {
+    data: teacherData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: [queryKeys.teacher, selected],
     queryFn: today,
     select: (res) => {
-      console.log(res.data);
-      return ["", "김도경", "박현아", "조영준"];
+      return res?.data?.sort((i: any, j: any) => i.floor - j.floor);
     },
   });
 
@@ -31,13 +35,22 @@ export const Teacher = () => {
         <Calendar onSelect={dateFn} />
         <Text type={["subTitle", 3, "M"]}>{selected} 자습감독 선생님</Text>
         <Box color={["primary", 1000]}>
+          <HiddenView data={isLoading}>
+            <Text type={["body", 1]}>불러오는 중입니다.</Text>
+          </HiddenView>
+          <HiddenView data={isError}>
+            <Text type={["body", 1]}>자습감독이 없습니다.</Text>
+          </HiddenView>
           <View style={{ gap: 30 }}>
             {teacherData?.map(
-              (item, index) =>
+              (item: any) =>
                 item !== "" && (
-                  <View key={index} style={{ flexDirection: "row", gap: 20 }}>
-                    <Text type={["label", 1]}>{index + 1}층</Text>
-                    <Text type={["label", 1]}>{item} 선생님</Text>
+                  <View
+                    key={item.floor}
+                    style={{ flexDirection: "row", gap: 20 }}
+                  >
+                    <Text type={["label", 1]}>{item.floor}층</Text>
+                    <Text type={["label", 1]}>{item.teacher_name} 선생님</Text>
                   </View>
                 )
             )}
