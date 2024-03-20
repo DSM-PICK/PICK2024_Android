e85bbc82bbd66e42e34293c6dfa9be6a10ef2602
six-standard <dbrrl1127@gmail.com>
2024-03-21 00:15:28 +0900

    chore: 주말급식 api 변경

--- app/api/apply/weekendMeal.ts
+++ app/api/apply/weekendMeal.ts
@@ -2,7 +2,7 @@ import { instance } from "../instance";
 import { path } from "../path";
 
 export const weekendMeal = async (id: string) => {
-  return await instance.patch(`${path.weekendMeal}/status?status=${id}`);
+  return await instance.patch(`${path.weekendMeal}/my-status?status=${id}`);
 };
 
 export const weekendMealMy = async () => {
