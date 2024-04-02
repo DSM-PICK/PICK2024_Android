44d2b3e53da85214a3075c01cba442de9e0a02b6
six-standard <dbrrl1127@gmail.com>
2024-04-02 23:41:23 +0900

    feat: 외출증 디자인 변경

--- app/screens/Pass/components/Label.tsx
+++ app/screens/Pass/components/Label.tsx
@@ -9,9 +9,9 @@ interface PropType {
 
 export default function Label({ title, children }: PropType) {
   return (
-    <View style={{ gap: 3 }}>
+    <View style={{ gap: 6 }}>
       <Text type={["label", 1]}>{title}</Text>
-      <Box color={["primary", 1200]}>
+      <Box color={["primary", 1000]}>
         <Text type={["subTitle", 3, "M"]} color={["neutral", 100]}>
           {children}
         </Text>
--- app/screens/Pass/index.tsx
+++ app/screens/Pass/index.tsx
@@ -1,26 +1,43 @@
 import { Image, StyleSheet, View } from "react-native";
-import { useQueryClient } from "@tanstack/react-query";
-import Label from "@/screens/Pass/components/Label";
+import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "@/constants";
-import { Layout, Box } from "@layouts";
+import { Layout } from "@layouts";
 import { Text } from "@commonents";
+import { getOut, getReturn } from "@/api";
+import { AxiosResponse } from "axios";
+import { Label } from "./components";
 
-const types = {
-  out: {
-    name: "외출",
-    color: ["primary", 500],
-  },
-  home: {
-    name: "조기 귀가",
-    color: ["tertiary", 500],
+const names = {
+  out: "외출",
+  home: "조기 귀가",
+};
+
+const placeholderData: AxiosResponse<any, any> = {
+  headers: {},
+  config: {},
+  status: 200,
+  statusText: "",
+  data: {
+    grade: 1,
+    class_num: 1,
+    num: 11,
+    username: "박수현",
+    start_time: "13:13:13",
+    end_time: "13:13:13",
   },
 };
 
 export const Pass = ({ route }) => {
-  const queryClient = useQueryClient();
-  const data = queryClient.getQueryData(queryKeys.anyApply)[1];
-  const { grade, class_num, num, username, start_time, end_time } = data;
   const { type } = route.params;
+
+  const { data } = useQuery({
+    queryKey: queryKeys.apply,
+    queryFn: type === "out" ? getOut : getReturn,
+    select: (res) => res?.data,
+    placeholderData,
+  });
+
+  const { grade, class_num, num, username, start_time, end_time } = data;
 
   const formDate = (time: string) => time.split(":").splice(0, 2).join(":");
   const user = `${grade}${class_num}${num} ${username}`;
@@ -30,39 +47,35 @@ export const Pass = ({ route }) => {
 
   return (
     <Layout name="외출증">
-      <View style={{ gap: 10 }}>
-        <View style={styles.titleContainer}>
-          <Text type={["subTitle", 1, "M"]}>{user}</Text>
-          <Text type={["subTitle", 1, "M"]} color={types[type].color}>
-            {types[type].name}
-          </Text>
+      <View
+        style={{
+          height: "90%",
+          position: "absolute",
+          alignSelf: "center",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
+        <View style={styles.detailContainer}>
+          <Text type={["heading", 6, "M"]}>{user}</Text>
+          <View style={{ gap: 20 }}>
+            <Label title={`${names[type]} 시간`}>{time}</Label>
+            <Label title="사유">{data.reason}</Label>
+            <Label title="확인 교사">{data.teacher_name + " 선생님"}</Label>
+            <Image
+              source={require("@/assets/Out.png")}
+              style={{ width: "100%", height: 68 }}
+            />
+          </View>
         </View>
-        <Box color={["primary", 1200]}>
-          <Image
-            style={{
-              width: "100%",
-              aspectRatio: 1,
-              resizeMode: "contain",
-            }}
-            source={{ uri: `data:image/jpeg;base64,${data?.image}` }}
-          />
-        </Box>
-        <Label title="외출 시간">{time}</Label>
-        <Label title="사유">{data.reason}</Label>
-        <Label title="확인 교사">{data.teacher_name + " 선생님"}</Label>
       </View>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  qrElement: {
-    width: "100%",
-    borderRadius: 4,
+  detailContainer: {
+    gap: 40,
+    paddingVertical: 40,
   },
 });
