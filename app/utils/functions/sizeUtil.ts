34564c6ed3f5d85bf0ab9b2896df8bbf81cd5cfa
six-standard <dbrrl1127@gmail.com>
2024-04-05 19:51:42 +0900

    refactor: sizeUtil 미사용 코드 주석처리

--- app/utils/functions/sizeUtil.ts
+++ app/utils/functions/sizeUtil.ts
@@ -1,7 +1,6 @@
 import { create } from "react-native-pixel-perfect";
-import { Dimensions } from "react-native";
-
-const { width, height } = Dimensions.get("window");
+// import { Dimensions } from "react-native";
+// const { width, height } = Dimensions.get("window");
 
 const size = {
   width: 390,
