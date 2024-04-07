76f319162df6f93bb03b135956b861c650f2c37b
six-standard <dbrrl1127@gmail.com>
2024-04-07 10:49:59 +0900

    refactor: 외출 신청 화면 리팩토링

--- app/screens/Out/index.tsx
+++ app/screens/Out/index.tsx
@@ -2,28 +2,13 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { View } from "react-native";
 import { useState } from "react";
 import { Label, TimePicker, PickerBox } from "./components";
-import { Text, Input, Modal } from "@commonents";
-import { Layout, HiddenView } from "@layouts";
-import { applyOut, applyReturn } from "@/api";
+import { Layout, TernaryView } from "@layouts";
+import { Text, Input } from "@commonents";
+import { post, useToast } from "@/utils";
 import { queryKeys } from "@/constants";
-import { AxiosError } from "axios";
-import { useToast } from "@/utils";
-
-const types = {
-  외출: {
-    title: "외출",
-    label: "외출",
-  },
-  조기귀가: {
-    title: "조기 귀가",
-    label: "귀가",
-  },
-};
 
 export const Out = ({ navigation, route }) => {
   const queryClient = useQueryClient();
-  const { type } = route.params;
-  const [sucVisible, setSucVisible] = useState(false);
   const [out, setOut] = useState({
     start_time: undefined,
     end_time: undefined,
@@ -34,30 +19,22 @@ export const Out = ({ navigation, route }) => {
     "",
   ]);
   const { start_time: start, end_time: end } = out;
-  const { title, label } = types[type];
+  const { type } = route.params;
   const toast = useToast();
 
-  const isOut = (value: any[]) => (title === "외출" ? value[0] : value[1]);
+  const isOut = (item1: any, item2: any) => (type === "외출" ? item1 : item2);
 
   const { mutate: outMutate } = useMutation({
-    mutationFn: () => {
-      if (title === "외출") {
-        return applyOut(out);
-      } else {
-        return applyReturn(out);
-      }
+    mutationFn: () => post(isOut("/out", "/early-return/create"), out),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: queryKeys.anyApply });
+      await navigation.reset({ routes: [{ name: "홈" as never }] });
+      toast.success(`${type} 신청이 완료되었습니다.`);
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeys.anyApply });
-      setSucVisible(true);
-    },
-    onError: (err: AxiosError) => {
-      if (err.response?.status === 409) {
-        toast.error("이미 신청되었습니다");
-      } else {
-        toast.error("오류가 발생했습니다.");
-      }
-    },
+    onError: ({ status }: any) =>
+      toast.error(
+        status === "409" ? "이미 신청되었습니다" : "오류가 발생했습니다"
+      ),
   });
 
   const handleChange = (item: any, type: string) => {
@@ -66,35 +43,40 @@ export const Out = ({ navigation, route }) => {
 
   return (
     <Layout
-      name={title + " 신청"}
+      name={type + " 신청"}
       onDone={outMutate}
-      isDone={isOut([start && end, start]) && out.reason !== ""}
+      isDone={isOut(start && end, start) && out.reason !== ""}
     >
       <View style={{ gap: 40 }}>
-        <Label title={`희망 ${label} 시간을 적어주세요`}>
-          <HiddenView data={title === "외출"}>
-            <PickerBox
-              setVisible={() => setPickVisible([true, "start_time"])}
-              time={out.start_time}
-              placeholder="출발 시간"
-            />
-            <Text type={["subTitle", 1, "M"]}>~</Text>
-            <PickerBox
-              setVisible={() => setPickVisible([true, "end_time"])}
-              time={end}
-              placeholder="도착 시간"
-            />
-          </HiddenView>
-          <HiddenView data={title !== "외출"}>
-            <PickerBox
-              full
-              setVisible={() => setPickVisible([true, "start_time"])}
-              time={start}
-              placeholder="출발 시간"
-            />
-          </HiddenView>
+        <Label title={`희망하는 ${type} 시간을 적어주세요`}>
+          <TernaryView
+            data={type === "외출"}
+            onTrue={
+              <>
+                <PickerBox
+                  setVisible={() => setPickVisible([true, "start_time"])}
+                  time={out.start_time}
+                  placeholder="출발 시간"
+                />
+                <Text type={["subTitle", 1, "M"]}>~</Text>
+                <PickerBox
+                  setVisible={() => setPickVisible([true, "end_time"])}
+                  time={end}
+                  placeholder="도착 시간"
+                />
+              </>
+            }
+            onFalse={
+              <PickerBox
+                full
+                setVisible={() => setPickVisible([true, "start_time"])}
+                time={start}
+                placeholder="출발 시간"
+              />
+            }
+          />
         </Label>
-        <Label title={`${label} 사유를 적어주세요`}>
+        <Label title={`${type} 사유를 적어주세요`}>
           <Input
             multiLine={4}
             onChange={({ text }) => {
@@ -110,16 +92,6 @@ export const Out = ({ navigation, route }) => {
         setVisible={setPickVisible}
         onDone={handleChange}
       />
-      <Modal
-        visible={sucVisible}
-        setVisible={setSucVisible}
-        type={3}
-        onAccept={() => {
-          navigation.reset({ routes: [{ name: "홈" as never }] });
-        }}
-      >
-        <Text type={["subTitle", 3, "M"]}>{title} 신청이 완료 되었습니다</Text>
-      </Modal>
     </Layout>
   );
 };
