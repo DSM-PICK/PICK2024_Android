15562d889e27ad5ed3c6e78810152bd83654962f
six-standard <dbrrl1127@gmail.com>
2024-04-07 11:04:39 +0900

    refactor: constant 데이터 정리

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,4 @@
-export { queryKeys } from "./queryKeys";
-export * from "./components";
+export * from "./queryKeys";
 
 const size = 10;
 
@@ -7,11 +6,14 @@ export const hitSlop = { top: size, left: size, right: size, bottom: size };
 
 export const defaultData = {
   name: "박수현",
-  birth_day: ["2024", "02", "24"],
+  birth_day: "2024-02-24",
   grade: 1,
   class_num: 4,
   num: 8,
   account_id: "soohyeon",
+  classRoom: "2-1",
+  start_time: "08:30",
+  end_time: "16:20",
 };
 
 export const months = [
